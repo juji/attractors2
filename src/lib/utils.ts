992bb5b0f230cbe81a5f55a20c56cb3c56470f11
juji <jujiyangasli@gmail.com>
2023-11-18 20:02:33 +0700

    refactor

---
+++ src/lib/utils.ts
@@ -0,0 +1,9 @@
+
+
+export function githubRepo(){
+  return 'https://github.com/juji/attractors2'
+}
+
+export function githubPlotter( file: string ){
+  return `https://github.com/juji/attractors2/blob/main/src/lib/plotters/${file}`
+}
